a19783b135fa9c2aa3d61d10f315aa05691a61cc
Noah Lubko <nlubko@gmail.com>
2019-07-14 18:49:35 +0300

    move unit test

--- src/app/todo/todo.component.unit.spec.ts
+++ src/app/todo/todo.component.unit.spec.ts
@@ -7,6 +7,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TodoComponent } from './todo.component';
 
 import { TodoService } from './todo.service';
+
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/from';
+
+import { ITodo } from '@core/interface';
+import { todoStatus } from '@core/enum';
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
@@ -25,5 +31,32 @@ describe('TodoComponent', () => {
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('initTodos - should call to getTodos and init todos with the results', () => {
+    const service = TestBed.get(TodoService);
+    const todos: ITodo[] = [{ id: 1, status: todoStatus.uncompleted, text: 'some todo' }];
+
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([todos]);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos.length).toBe(1);
+  });
+
+  it('addTodo - should call add todo and add the result to the todos', () => {
+    const service = TestBed.get(TodoService);
+
+    const todo: ITodo = { id: 1, text: 'new todo', status: todoStatus.uncompleted };
+
+    spyOn(service, 'addTodo').and.callFake(() => {
+      return Observable.from([todo]);
+    });
+
+    component.addTodo({ keyCode: 13 }, 'new todo');
+
+    expect(component.todos.length).toBe(1);
   });
 });
