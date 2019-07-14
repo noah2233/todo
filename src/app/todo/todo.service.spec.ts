0943c9d41d3e1590a51a97672a24af6c30128053
Noah Lubko <nlubko@gmail.com>
2019-07-14 11:05:01 +0300

    remove tests

--- src/app/todo/todo.service.spec.ts
+++ src/app/todo/todo.service.spec.ts
@@ -1,12 +1,13 @@
 /* tslint:disable:no-unused-variable */
-
 import { TestBed, async, inject } from '@angular/core/testing';
 import { TodoService } from './todo.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TodoService]
+      providers: [TodoService],
+      schemas: [NO_ERRORS_SCHEMA]
     });
   });
 
